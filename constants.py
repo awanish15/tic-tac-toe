4386b094f2997a44ea599af83e1e37cb7f8a7374
awanish15 <93979235+awanish15@users.noreply.github.com>
2022-07-10 03:00:20 +0530

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,27 @@
+# ---------
+# CONSTANTS
+# ---------
+
+# --- PIXELS ---
+
+WIDTH = 600
+HEIGHT = 600
+
+ROWS = 3
+COLS = 3
+SQSIZE = WIDTH // COLS
+
+LINE_WIDTH = 10
+CIRC_WIDTH = 10
+CROSS_WIDTH = 20
+
+RADIUS = SQSIZE // 3
+
+OFFSET = 50
+
+# --- COLORS ---
+
+BG_COLOR = (178, 217, 255)
+LINE_COLOR = (255, 227, 178)
+CIRC_COLOR = (239, 231, 200)
+CROSS_COLOR = (66, 66, 66)
